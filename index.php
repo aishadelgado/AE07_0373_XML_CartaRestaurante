4eab7ce8d21137867842022e8a16906360288b20
Aisha <454617.joan23@fje.edu>
2026-03-26 13:35:01 +0100

    CSS + errores corregidos

--- index.php
+++ index.php
@@ -21,10 +21,10 @@ $tipos = [
 $infoTipos = [
     'tapas' => ['icono' => 'fa-mug-hot', 'nombre' => 'Tapas'],
     'bocadillos_calientes' => ['icono' => 'fa-bread-slice', 'nombre' => 'Bocadillos Calientes'],
-    'bocadillos_frios' => ['icono' => 'fa-sandwich', 'nombre' => 'Bocadillos Fríos'],
+    'bocadillos_frios' => ['icono' => 'fa-bread-slice', 'nombre' => 'Bocadillos Fríos'],
     'platos_combinados' => ['icono' => 'fa-utensils', 'nombre' => 'Platos Combinados'],
     'bandejas' => ['icono' => 'fa-cheese', 'nombre' => 'Bandejas para Compartir'],
-    'torradas' => ['icono' => 'fa-baguette', 'nombre' => 'Torradas']
+    'torradas' => ['icono' => 'fa-bread-slice', 'nombre' => 'Torradas']
 ];
 
 $iconosCaracteristicas = [
@@ -71,8 +71,11 @@ function esDestacado($plato) {
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=yes">
     <title>Bar La Tasca | Carta Tradicional</title>
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Lato:wght@300;400;700&family=Playfair+Display:wght@700&display=swap">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
-    <link rel="stylesheet" href="./css/style.css">
+    <link rel="stylesheet" href="css/style.css">
 </head>
 <body>
 
